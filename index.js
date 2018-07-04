91a0df5bda95e53df36423509b97658ed960efe9
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-07-04 01:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,9 +41,11 @@ function checkCollision(rock) {
      * 3. The rock's left edge is < the DODGER's right edge,
      *    and the rock's right edge is > the DODGER's right edge
      */
-    return ((rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
-     else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
-      else if (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge))
+    return (
+      (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) ||
+      else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+      else if (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge)
+    )
   }
 }
 
